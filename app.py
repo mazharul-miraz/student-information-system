b68c2925ec839576cbc90fa989d91a6a0116fa18
mazharul.miraz <mazharul.miraz@outlook.com>
2019-10-02 02:27:16 +0530

    database init

--- app.py
+++ app.py
@@ -1,9 +1,67 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template, url_for, request
+from pymongo import MongoClient
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+db = client.sisdb
+print(db)
+
+
+
+
+app = Flask(__name__)
+@app.route('/',methods=['POST', 'GET'])
+def main():
+	if request.method == 'POST':
+		return Insertdata(request)
+	else:
+		return render_template('index.html')
+
+def Insertdata():
+	Firstname = request.form['firstname']
+	Lastname = request.form['lastname']
+	Regno = request.form['regno']
+	Year = request.form['year']
+	Date = request.form['date']
+	Domain = request.form['domain']
+	Address = request.form['address']
+
+
+
+
+
+
+
+@app.route('/update')
+def update():
+   return render_template('update.html')
+
+
+
+
+@app.route('/delete')
+def delete():
+   return render_template('delete.html')
+   
+
+
+@app.route('/search')
+def search():
+   return render_template('search.html')
+
+
+@app.route('/viewall')
+def viewall():
+   return 'all record goes here'
+
+
+
+
+
+
+
 
 
 
