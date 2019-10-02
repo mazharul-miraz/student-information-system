82dfb640dca725888042a705534619e9330031d6
mazharul.miraz <mazharul.miraz@outlook.com>
2019-10-03 00:09:14 +0530

    insert fixed

--- app.py
+++ app.py
@@ -1,34 +1,51 @@
 from flask import Flask
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, redirect
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 
 client = MongoClient()
 client = MongoClient('localhost', 27017)
 db = client.sisdb
-print(db)
-
-
 
 
 app = Flask(__name__)
-@app.route('/',methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def main():
-	if request.method == 'POST':
-		return Insertdata(request)
-	else:
-		return render_template('index.html')
-
-def Insertdata():
-	Firstname = request.form['firstname']
-	Lastname = request.form['lastname']
-	Regno = request.form['regno']
-	Year = request.form['year']
-	Date = request.form['date']
-	Domain = request.form['domain']
-	Address = request.form['address']
+    if request.method == 'POST':
+        return Insertdata(request)
+    else:
+        return render_template('index.html')
 
 
+def Insertdata(request):
+    print('data post')
+    Firstname = request.form['firstname']
+    Lastname = request.form['lastname']
+    Regno = request.form['regno']
+    Year = request.form['year']
+    Date = request.form['date']
+    Domain = request.form['domain']
+    Address = request.form['address']
+
+    Olduser = db.user.find_one({
+        'regno': Regno,
+        # fieldName: FieldData
+    })
+
+    if Olduser != None:
+        return 'This Email ID Already Exist'
+    else:
+        db.user.insert_one({
+            'firstname': Firstname,
+            'lastname': Lastname,
+            'regno': Regno,
+            'year': Year,
+            'date': Date,
+            'domain': Domain,
+            'address': Address,
+        })
+    return redirect(url_for('/'))
 
 
 
@@ -36,34 +53,23 @@ def Insertdata():
 
 @app.route('/update')
 def update():
-   return render_template('update.html')
-
-
+    return render_template('update.html')
 
 
 @app.route('/delete')
 def delete():
-   return render_template('delete.html')
-   
+    return render_template('delete.html')
 
 
 @app.route('/search')
 def search():
-   return render_template('search.html')
+    return render_template('search.html')
 
 
 @app.route('/viewall')
 def viewall():
-   return 'all record goes here'
-
-
-
-
-
-
-
-
+    return 'all record goes here'
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
