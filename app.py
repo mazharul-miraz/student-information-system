aefabc33b1ec6830f2642ee150f95c047093ff2e
mazharul-miraz <mazharul.miraz@outlook.com>
2019-11-22 00:03:13 +0530

    app structure redesign

--- app.py
+++ app.py
@@ -1,123 +1,67 @@
 from flask import Flask
 from flask import render_template, url_for, request, redirect
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-
-
-client = MongoClient()
-client = MongoClient('localhost', 27017)
-db = client.sisdb
-
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://localhost:27017/')
+db = client.sisdb
+collection = db.sisdb
+
+
+#USER LOGIN
 @app.route('/', methods=['POST', 'GET'])
-def main():
-    if request.method == 'POST':
-        return Insertdata(request)
-    else:
-        return render_template('index.html')
-
-
-def Insertdata(request):
-    print('data post')
-    Firstname = request.form['firstname']
-    Lastname = request.form['lastname']
-    Regno = request.form['regno']
-    Year = request.form['year']
-    Date = request.form['date']
-    Domain = request.form['domain']
-    Address = request.form['address']
-
-    Olduser = db.user.find_one({
-        'regno': Regno,
-        # fieldName: FieldData
-    })
-
-    if Olduser != None:
-        return 'This Email ID Already Exist'
-    else:
-        db.user.insert_one({
-            'firstname': Firstname,
-            'lastname': Lastname,
-            'regno': Regno,
-            'year': Year,
-            'date': Date,
-            'domain': Domain,
-            'address': Address,
-        })
-    return redirect('/')
-
-
-@app.route('/update', methods=['POST', 'GET'])
-def update():
-    if request.method == 'POST':
-        return SearchResult(request)
-    else:
-        return render_template('update.html')
-
-
-@app.route('/delete', methods=['POST', 'GET'])
-def delete():
-    if request.method == 'POST':
-        return userdel(request)
-    else:
-        return render_template('delete.html')
-
-
-def userdel(request):
-    Regno = request.form['regno']
-
-    deluser = db.user.find_one({
-        'regno': Regno,
-    })
-    if deluser != None:
-        db.user.delete_one({
-            'regno': Regno
-        })
-        return redirect('/delete')
-    else:
-        return 'No user found'
-
-
-@app.route('/search', methods=['POST', 'GET'])
-def search():
-    if request.method == 'POST':
-        return SearchResult(request)
-    else:
-        return render_template('search.html')
-
-
-def SearchResult(request):
-
-    Regno = request.form['regno']
-    searchData = db.user.find_one({
-        'regno': Regno,
-    })
-    if searchData == None:
-        return 'no data found'
-    else:
-        print(searchData)
-        return render_template('search.html', user=searchData)
-
-
-@app.route('/viewall')
-def viewall():
-    users = db.user.find()
-    user_list = []
-    for i in users:
-        user_list.append(i)
-    return render_template('viewall.html', user=user_list)
-
-
-@app.route('/viewall')
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        return
+    else:
+        return render_template('login.html')
 
 
+# USER DASHBOARD
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+
+# USER DELETE
+@app.route('/user_del')
+def user_del():
+    return render_template('user_del.html')
+
+
+# USER SEARCH
+@app.route('/user_src')
+def user_src():
+    return render_template('user_src.html')
+
+
+# USER UPDATE
+@app.route('/user_update')
+def user_update():
+    return render_template('user_update.html')
+
+
+# SUPER USER
 @app.route('/sudo')
 def sudo():
     return render_template('sudo.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# VIEW ALL USER
+@app.route('/all_user')
+def view_all():
+    return render_template('all_user.html')
+
+
+# ADD A USER
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        return CreateUser()
+    else:
+        print('something went wrong')
+
+
+app.run(debug=True)
